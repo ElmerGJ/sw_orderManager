1cee658e4ad6214357be6a375ff1bbb52e88ec43
Elmer GJ <elmerdaniel1120@hotmail.com>
2023-08-16 13:24:57 -0500

    Commiting progress - Fronted

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import BottomMenu from './components/Nav/BottomMenu';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Deliveries from './pages/Deliveries';
+import Requests from './pages/Requests';
+import Stats from './pages/Stats';
+import Settings from './pages/Settings';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Working on it....!!!
-        </p>
-        
-      </header>
-    </div>
+  return (  
+      <div>
+          <Router>
+              <div>
+              <Routes>
+                  <Route path="/deliveries" element={<Deliveries/>}/>
+                  <Route path='/requests' element={<Requests/>}/>
+                  <Route path='/stats' element={<Stats/>}/>
+                  <Route path='/settings' element={<Settings/>}/>
+              </Routes>
+            </div>
+            <BottomMenu />
+          </Router>
+      </div>
   );
 }
 
