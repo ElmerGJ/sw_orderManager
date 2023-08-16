aaedc8819bdf1923d5b03b1c5113a0cf5e87bb94
Elmer GJ <elmerdaniel1120@hotmail.com>
2023-08-16 13:30:22 -0500

    Updating folders

---
+++ src/pages/Deliveries.js
@@ -0,0 +1,17 @@
+// import { Link } from 'react-router-dom';
+import BottomMenu from '../components/Nav/BottomMenu';
+
+function Deliveries() {
+  return (
+    <div>
+      <h1>Deliveries</h1>
+      
+
+        <div>
+            <BottomMenu />
+        </div>
+    </div>
+  );
+}
+
+export default Deliveries;
