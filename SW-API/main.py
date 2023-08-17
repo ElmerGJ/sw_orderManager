d961a76e5d20d9426696bb8ef0c242c2c76b8791
Victor <victorjos2001@hotmail.com>
2023-08-17 17:41:28 -0500

    agrego lo que e hecho hasta ahora

---
+++ SW-API/main.py
@@ -0,0 +1,5 @@
+from flask import Flask
+from API import app  # Importa la instancia de app desde el archivo api_module
+
+if __name__ == '__main__':
+    app.run()
--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './components/Login';
+import LandingPage from './components/LandingPage';
 
-function App() {
+const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  // Check for token in local storage on initial render
+  useEffect(() => {
+      const token = localStorage.getItem('token');
+      if (token) {
+          setLoggedIn(true);
+      }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Working on it....!!!
-        </p>
-        
-      </header>
-    </div>
+      <Router>
+          <Routes>
+              <Route path="/login" element={loggedIn ? <Navigate to="/landing" /> : <Login setLoggedIn={setLoggedIn} />} />
+              <Route path="/landing" element={loggedIn ? <LandingPage setLoggedIn={setLoggedIn} /> : <Navigate to="/login" />} />
+          </Routes>
+      </Router>
   );
-}
+};
 
 export default App;
