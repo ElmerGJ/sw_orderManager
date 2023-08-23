f51edf54e27eb2d00527fbf4ad821bffd817894d
Victor <victorjos2001@hotmail.com>
2023-08-23 10:27:26 -0500

    cambios en ruta cliente

--- SW-API/app/routes/cliente.py
+++ SW-API/app/routes/cliente.py
@@ -6,55 +6,38 @@ cliente_bp = Blueprint('cliente', __name__)
 
 @cliente_bp.route('/api/cliente/crear-solicitud', methods=['POST'])
 def crear_solicitud():
-    # Aquí debes implementar la lógica para crear una nueva solicitud
-    idSolicitante = request.json['idSolicitante']
-
+    data = request.json
+    idSolicitante = data['idSolicitante']
+    
     solicitante = Usuarios.query.get(idSolicitante)
     if solicitante is None:
         return jsonify({"error": "Solicitante no encontrado"}), 404
-
-    cliente = request.json['cliente']
-    fecha_entrega = datetime.strptime(request.json['fecha_entrega'], '%Y-%m-%d').date()
-    lugar = request.json['lugar']
-    nombre_recibe = request.json['nombre_recibe']
-    telefono_recibe = request.json['telefono_recibe']
-    cantidad_tanques = request.json['cantidad_tanques']
-    cantidad_galones = request.json['cantidad_galones']
-    cantidad_cuartos = request.json['cantidad_cuartos']
-    cantidad_otros = request.json['cantidad_otros']
-    fecha_solicitud = datetime.now().date()
-    cliente_credito = request.json['cliente_credito']
-    origen_producto = request.json['origen_producto']
-    preparacion = request.json['preparacion']
-    idRTV = request.json['idRTV']
-    idSolicitante = request.json['idSolicitante']
-    Estado = request.json['Estado']
+    
+    cliente = f"{solicitante.nombre} {solicitante.apellido}"
 
     nueva_solicitud = Solicitudes(
         cliente=cliente,
-        fecha_entrega=fecha_entrega,
-        lugar=lugar,
-        nombre_recibe=nombre_recibe,
-        telefono_recibe=telefono_recibe,
-        cantidad_tanques=cantidad_tanques,
-        cantidad_galones=cantidad_galones,
-        cantidad_cuartos=cantidad_cuartos,
-        cantidad_otros=cantidad_otros,
-        fecha_solicitud=fecha_solicitud,
-        cliente_credito=cliente_credito,
-        origen_producto=origen_producto,
-        preparacion=preparacion,
-        idRTV=idRTV,
+        fecha_entrega=datetime.strptime(data['fecha_entrega'], '%Y-%m-%d').date(),
+        lugar=data['lugar'],
+        nombre_recibe=data['nombre_recibe'],
+        telefono_recibe=data['telefono_recibe'],
+        cantidad_tanques=data['cantidad_tanques'],
+        cantidad_galones=data['cantidad_galones'],
+        cantidad_cuartos=data['cantidad_cuartos'],
+        cantidad_otros=data['cantidad_otros'],
+        fecha_solicitud=datetime.now().date(),
+        cliente_credito=data['cliente_credito'],
+        origen_producto=data['origen_producto'],
+        preparacion=data['preparacion'],
+        idRTV=idSolicitante,
         idSolicitante=idSolicitante,
-        Estado=Estado
+        Estado=data['Estado']
     )
-
+    
     db.session.add(nueva_solicitud)
     db.session.commit()
-
+    
     return jsonify({"mensaje": "Solicitud creada exitosamente"})
-    # Código para crear una solicitud
-pass
 
 @cliente_bp.route('/api/cliente/mis-solicitudes', methods=['GET'])
 def mis_solicitudes():
